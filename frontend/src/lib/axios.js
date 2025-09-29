6b573d9c3b4846717b057da9ec88ae83cdde94f3
Prayer07 <ezenwaokechukwu3@gmail.com>
2025-09-29 21:05:00 +0100

    omo2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://localhost:5173" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
